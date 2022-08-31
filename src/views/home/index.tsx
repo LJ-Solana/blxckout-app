2a72097e799db7d217012380855e34718ad243ed
LJ <lukejudges@LJs-MacBook-Pro.local>
2022-08-31 17:05:16 +0100

    home page formatting

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -32,21 +32,6 @@ export const HomeView: FC = ({ }) => {
     <div className="bg-indigo-600">
     <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
       <div className="flex flex-wrap items-center justify-between">
-        <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
-          <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50">Learn more</a>
-        </div>
-        <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-          <button type="button" className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
-            <span className="sr-only">Dismiss</span>
-
-            <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </div>
-    </div>
       <div className="md:hero-content flex flex-col">
       <img src="Blxckout_Words_Logo.png" height="300" width="450"></img>
 
