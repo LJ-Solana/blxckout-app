1bad44b8ba4caf2631d33adead37c36239652fd5
LJ <lukejudges@LJs-MacBook-Pro.local>
2022-08-31 18:10:48 +0100

    Synx page updates

--- src/views/synx/index.tsx
+++ src/views/synx/index.tsx
@@ -20,7 +20,7 @@ export const SynxView: FC = ({ }) => {
               <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
                 <div className="-mt-6">
                   <div>
-                    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                    <span className="inline-flex items-center justify-center rounded-md bg-primary-focus p-3 shadow-lg">
 
                       <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
@@ -37,7 +37,7 @@ export const SynxView: FC = ({ }) => {
               <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
                 <div className="-mt-6">
                   <div>
-                    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                    <span className="inline-flex items-center justify-center rounded-md bg-primary-focus p-3 shadow-lg">
 
                       <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
@@ -54,7 +54,7 @@ export const SynxView: FC = ({ }) => {
               <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
                 <div className="-mt-6">
                   <div>
-                    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                    <span className="inline-flex items-center justify-center rounded-md bg-primary-focus p-3 shadow-lg">
 
                       <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12c0-1.232.046-2.453.138-3.662a4.006 4.006 0 013.7-3.7 48.678 48.678 0 017.324 0 4.006 4.006 0 013.7 3.7c.017.22.032.441.046.662M4.5 12l-3-3m3 3l3-3m12 3c0 1.232-.046 2.453-.138 3.662a4.006 4.006 0 01-3.7 3.7 48.657 48.657 0 01-7.324 0 4.006 4.006 0 01-3.7-3.7c-.017-.22-.032-.441-.046-.662M19.5 12l-3 3m3-3l3 3" />
@@ -63,23 +63,6 @@ export const SynxView: FC = ({ }) => {
                   </div>
                   <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">Perfect Reporting</h3>
                   <p className="mt-5 text-base text-gray-500">Data confidentiality, integrity and availability. Achieve regulatory compliance in any industry by having an immutable record of all your files.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="pt-6">
-              <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
-                <div className="-mt-6">
-                  <div>
-                    <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
-
-                      <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
-                      </svg>
-                    </span>
-                  </div>
-                  <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">Own your Data</h3>
-                  <p className="mt-5 text-base text-gray-500">Break free from big tech and ever-changing terms of service. Enjoy the benefits of total privacy, security, and extreme data redundancy.</p>
                 </div>
               </div>
             </div>
